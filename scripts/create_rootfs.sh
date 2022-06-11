a5dc3906d3d37414ed8fe49d02cb8e88525454f8
thatretrodev <thatretrodev@outlook.com>
2022-06-11 13:25:03 -0400

    Update build script

--- scripts/create_rootfs.sh
+++ scripts/create_rootfs.sh
@@ -15,39 +15,21 @@ mkdir -p $SANDWICH_OUTPUT
 
 if [ ! -d $SANDWICH/source ]
 then
-	mkdir -p $SANDWICH/source/tarball
-	mkdir -p $SANDWICH/source/decompressed
+	mkdir -p $SANDWICH/source/packages
 
-	# download tarballs
+	# download files
 
-	wget https://ftp.gnu.org/gnu/glibc/glibc-$GLIBC_VERSION.tar.gz -O $SANDWICH/source/tarball/glibc-$GLIBC_VERSION.tar.gz
-	wget https://www.busybox.net/downloads/busybox-$BUSYBOX_VERSION.tar.bz2 -O $SANDWICH/source/tarball/busybox-$BUSYBOX_VERSION.tar.bz2
+	wget https://github.com/SandwichOS/slice/releases/download/test1/slice -O $SANDWICH/source/slice
+
+	chmod +x $SANDWICH/source/slice
+
+	wget https://github.com/SandwichOS/slice/releases/download/test1/slice.slicepkg -O $SANDWICH/source/packages/slice.slicepkg
+	wget https://github.com/SandwichOS/core-packages/releases/download/test1/glibc.slicepkg -O $SANDWICH/source/packages/glibc.slicepkg
+	wget https://github.com/SandwichOS/core-packages/releases/download/test1/busybox.slicepkg -O $SANDWICH/source/packages/busybox.slicepkg
+	#wget https://github.com/SandwichOS/core-packages/releases/download/test1/coreutils.slicepkg -O $SANDWICH/source/packages/coreutils.slicepkg
 fi
 
-# compile glibc
-
-if [ ! -d $SANDWICH/source/decompressed/glibc-$GLIBC_VERSION ]
-then
-	tar -xvf $SANDWICH/source/tarball/glibc-$GLIBC_VERSION.tar.gz -C $SANDWICH/source/decompressed
-fi
-
-mkdir $SANDWICH/source/decompressed/glibc-$GLIBC_VERSION/build
-
-cd $SANDWICH/source/decompressed/glibc-$GLIBC_VERSION/build
-
-../configure --libdir=/lib/x86_64-linux-gnu --prefix=/ && make -j8 && make install DESTDIR=$SANDWICH_OUTPUT && ln -s lib $SANDWICH_OUTPUT/lib64
-
-cd $ORIGINAL_PATH
-
-# compile busybox
-
-if [ ! -d $SANDWICH/source/decompressed/busybox-$BUSYBOX_VERSION ]
-then
-	tar -xvf $SANDWICH/source/tarball/busybox-$BUSYBOX_VERSION.tar.bz2 -C $SANDWICH/source/decompressed
-fi
-
-cd $SANDWICH/source/decompressed/busybox-$BUSYBOX_VERSION
-
-make defconfig && make -j8 && make install && cp -r _install/* $SANDWICH_OUTPUT
-
-cd $ORIGINAL_PATH
+SLICE_DESTDIR=$SANDWICH_OUTPUT $SANDWICH/source/slice install $SANDWICH/source/packages/slice.slicepkg
+SLICE_DESTDIR=$SANDWICH_OUTPUT $SANDWICH/source/slice install $SANDWICH/source/packages/glibc.slicepkg
+SLICE_DESTDIR=$SANDWICH_OUTPUT $SANDWICH/source/slice install $SANDWICH/source/packages/busybox.slicepkg
+#SLICE_DESTDIR=$SANDWICH_OUTPUT $SANDWICH/source/slice install $SANDWICH/source/packages/coreutils.slicepkg
